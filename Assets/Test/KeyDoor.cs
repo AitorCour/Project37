69b6f711eb905204736212cdb21b1d96521717e9
DESKTOP-DQHGR6Q\aitor <aitorcour@gmail.com>
2018-10-18 12:14:59 +0200

    puerta llave completa. Win y Lose implementados

--- Assets/Test/KeyDoor.cs
+++ Assets/Test/KeyDoor.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class KeyDoor : MonoBehaviour 
 {
 	private PlayerBehaviour plBehaviour;
+	public GameObject TextPanel = null;
 	public int scene; //se introduce la scena a la que se quiere ir
 	private bool isInsideTrigger = false;
-	public int key = 1;
+	public int key = 1; //Con esto se podrá poner cuantas llaves se necesitará para la puerta
+	private bool isDoorOpen = false;
+	private bool MessageReaded = false;
+	public string message = "Hello World";
+	public Text eText;
 	void Start()
 	{
 		plBehaviour = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
@@ -17,13 +23,22 @@ public class KeyDoor : MonoBehaviour
 	{
 		if (isInsideTrigger && Input.GetKeyDown(KeyCode.C))
 		{
-			if(plBehaviour.Keys >= 1)
+			if(plBehaviour.Keys >= key)
 			{
-				Debug.Log ("Change Scene");
-				SceneManager.LoadScene(scene); //linea que hace que funcione
-				plBehaviour.LoseKeys(key);
+				//Debug.Log ("Change Scene");
+				SceneManager.LoadScene(scene); //cambio scene
+				plBehaviour.LoseKeys(key); //player pierde una o lo que se necesiten de llaves
+				isDoorOpen = true;
 			}
-			else Debug.Log("GetKeys");
+			else if(isDoorOpen) // utilizar esto para si vuelve a la habitación o lo que sea, que la puerta ya esté abierta
+			{
+				SceneManager.LoadScene(scene);
+			}
+			else if (MessageReaded)
+			{
+				ReadEnd();
+			}
+			else Read();
 		}
 	}
 
@@ -40,5 +55,20 @@ public class KeyDoor : MonoBehaviour
 		{
 			isInsideTrigger = false;
 		}
+	}
+	private void Read()
+	{
+		TextPanel.SetActive(true);
+		eText.text = message;
+		Debug.Log("reading");
+		MessageReaded = true;
+		Time.timeScale = 0;
+	}
+	private void ReadEnd()
+	{
+		TextPanel.SetActive(false);
+		Debug.Log("quit");
+		MessageReaded = false;
+		Time.timeScale = 1;
 	}
 }
