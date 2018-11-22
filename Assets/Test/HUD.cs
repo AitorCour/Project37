f6e4042780952fd40a276a488ce6602e4a0208f5
AitorCour <aitorcour@gmail.com>
2018-11-22 19:07:09 +0100

    numeritos de vida ammo en inv

--- Assets/Test/HUD.cs
+++ Assets/Test/HUD.cs
@@ -5,17 +5,27 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour 
 {
-	public Gun gun;
+	private Gun gun;
+	private PlayerBehaviour plBehaviour;
 	public Text ammo;
+	public Text ammoInv;
+	public Text bandages;
+	public Text keys;
+	public Text plLife;
 	// Use this for initialization
 	void Start () 
 	{
 		gun = GameObject.FindGameObjectWithTag("Weapon").GetComponent<Gun>();
+		plBehaviour = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		ammo.text = gun.currentAmmo + " / " + gun.Munition.ToString();
+		bandages.text = "Bandages: " + plBehaviour.Potions.ToString();
+		keys.text = "Keys: " + plBehaviour.Keys.ToString();
+		plLife.text = "Life: " + plBehaviour.PlayerLife.ToString();
+		ammoInv.text = gun.currentAmmo + " / " + gun.Munition.ToString();
 	}
 }
