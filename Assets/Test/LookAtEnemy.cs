f196851fcc208a9a260d06324c4f01a5b706961a
DESKTOP-DQHGR6Q\aitor <aitorcour@gmail.com>
2018-12-01 11:46:34 +0100

    ya no detecta los enemigos si hay paredes

--- Assets/Test/LookAtEnemy.cs
+++ Assets/Test/LookAtEnemy.cs
@@ -30,6 +30,14 @@ public class LookAtEnemy : MonoBehaviour
    private Quaternion lookAt;
    private Quaternion targetRotation;
 
+   //Detectar
+   //public Transform player;
+   //public float maxAngle;
+   public float maxRadius;
+
+   private bool isInFov = false;
+   public bool detected = false;
+
    void Start()
    {
 		InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -63,7 +71,8 @@ public class LookAtEnemy : MonoBehaviour
 
    void Update()
    {
-        if (EnemyInFieldOfView(fovStartPoint))
+		isInFov = inFOV(transform, target, maxAngle, maxRadius);
+        if (EnemyInFieldOfView(fovStartPoint) && isInFov)
         {
             //Vector3 direction = enemy.transform.position - transform.position;
 			//Vector3 direction = enemy[currentEnemy].position - transform.position;
@@ -89,7 +98,7 @@ public class LookAtEnemy : MonoBehaviour
             if (arms)
             {
                 // make arms point at the ground
-                Quaternion targetRotation = Quaternion.Euler(90, 0, 0);
+                Quaternion targetRotation = Quaternion.Euler(0, 0, 0);//Con el primer valor a 90, los brazos apuntarán al suelo
                 transform.localRotation = Quaternion.RotateTowards(
                 transform.localRotation, targetRotation, Time.deltaTime * lookSpeed);
             }
@@ -142,4 +151,67 @@ public class LookAtEnemy : MonoBehaviour
         }
    }
    //si el transform del player está mas cerca del transform del otro enemigo, cambio
+   private void OnDrawGizmos() //Dibujar el campo de visión
+   {
+		
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, maxRadius);
+
+		Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.up) * transform.forward * maxRadius;
+		Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.up) * transform.forward * maxRadius;
+
+		Gizmos.color = Color.blue;
+		Gizmos.DrawRay(transform.position, fovLine1);
+		Gizmos.DrawRay(transform.position, fovLine2);
+
+		if (!isInFov)
+		{
+			Gizmos.color = Color.red;
+			//detected = false;
+		}
+		else
+		{
+			Gizmos.color = Color.green;
+			//detected = true;
+		}
+		//Gizmos.DrawRay(transform.position, (target.position - transform.position).normalized * maxRadius);
+
+		Gizmos.color = Color.black;
+		Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
+   }
+
+   public static bool inFOV(Transform checkingObject, Transform target2, float maxAngle, float maxRadius)
+	{
+		Collider[] overlaps = new Collider[100];
+		int count = Physics.OverlapSphereNonAlloc(checkingObject.position, maxRadius, overlaps);
+
+		for(int i = 0; i < count + 1; i++)
+		{
+			if(overlaps[i] != null)
+			{
+				if(overlaps[i].transform == target2)
+				{
+					Vector3 directionBetween = (target2.position - checkingObject.position).normalized;
+					directionBetween.y *= 0;
+
+					float angle = Vector3.Angle(checkingObject.forward, directionBetween);
+
+					if(angle <= maxAngle)
+					{
+						Ray ray = new Ray(checkingObject.position, target2.position - checkingObject.position);
+						RaycastHit hit;
+
+						if(Physics.Raycast(ray, out hit, maxRadius))
+						{
+							if (hit.transform == target2)
+							{ 
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
