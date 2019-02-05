e6a025d3712cd6042d6de374776cdc173bd90a70
DESKTOP-DQHGR6Q\aitor <aitorcour@gmail.com>
2019-02-05 23:12:14 +0100

    Terminar de poner bien la escala del inv...  Consultar antes lo de todo el aire que sobra en la imagen del quaderno

--- Assets/scripts/SettingsMenu.cs
+++ Assets/scripts/SettingsMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 public class SettingsMenu : MonoBehaviour 
 {
-	public TMP_Dropdown resolutionDropdown;
+	/*public TMP_Dropdown resolutionDropdown;
 	Resolution[] resolutions;
 	void Start ()
 	{
@@ -29,7 +29,7 @@ public class SettingsMenu : MonoBehaviour
 		resolutionDropdown.AddOptions(options);
 		resolutionDropdown.value = currentResolutionIndex;
 		resolutionDropdown.RefreshShownValue();
-	}
+	}*/
 
 	/*public void SetResolution (int resolutionIndex)
 	{
@@ -72,5 +72,9 @@ public class SettingsMenu : MonoBehaviour
 	public void SetQualityLow ()
 	{
 		QualitySettings.SetQualityLevel(3);
+	}
+	public void SetFullscreen (bool isFullscreen)
+	{
+		Screen.fullScreen = isFullscreen;
 	}
 }
